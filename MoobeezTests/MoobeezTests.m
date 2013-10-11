1a8c6f2e1f8a1cf3b211173bed089efb2004e17d
Radu Banea <radubanea@cronos-technologies.com>
2013-10-11 15:47:36 +0300

    Initial Commit

---
+++ MoobeezTests/MoobeezTests.m
@@ -0,0 +1,34 @@
+//
+//  MoobeezTests.m
+//  MoobeezTests
+//
+//  Created by Radu Banea on 10/11/13.
+//  Copyright (c) 2013 Goggzy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoobeezTests : XCTestCase
+
+@end
+
+@implementation MoobeezTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
